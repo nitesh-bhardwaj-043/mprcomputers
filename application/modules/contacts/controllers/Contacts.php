2bfbf08624e4609178a77610dd0dcbefb9a997ba
nitesh-bhardwaj-043 <nitesh123bhardwaj123@gmail.com>
2025-11-05 00:52:56 +0530

    minor changes in contacts

--- application/modules/contacts/controllers/Contacts.php
+++ application/modules/contacts/controllers/Contacts.php
@@ -1,9 +1,10 @@
 <?php if (! defined('BASEPATH')) exit('No direct script access allowed');
 class Contacts extends MX_Controller
 {
-    function __construct(){
+    function __construct()
+    {
         parent::__construct();
-     $this->load->database();
+        //  $this->load->database();
     }
     function index()
     {
@@ -13,87 +14,21 @@ class Contacts extends MX_Controller
         $data['view_file'] = "contacts";
         echo Modules::run('template/layout2', $data);
     }
-  
-  
-    function bookings()
-    {
-        $this->load->library('form_validation');
-        $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('phone', 'Mobile', 'required|trim|numeric|exact_length[10]');
-        $this->form_validation->set_rules('email', "Email", 'trim|valid_email');
-        $this->form_validation->set_rules('mfrom', 'Moving From', 'required|trim');
-        $this->form_validation->set_rules('mto', 'Moving To', 'required|trim');
-        $this->form_validation->set_rules('message', 'Message', 'trim');
-        if ($this->form_validation->run() == true) {
-            $this->load->model('contacts_mdl');
-            $check = $this->contacts_mdl->bookings();
-            if ($check == true) {
-          echo "1";
-            }
-        } else {
-            echo "<div class='alert alert-danger'>" . validation_errors() . "</div>";
-        }
-    }
-    function faq()
-    {
-        $this->load->library('form_validation');
-        $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('phone', 'Mobile', 'required|trim|numeric|exact_length[10]');
-        if ($this->form_validation->run() == true) {
-            $this->load->model('contacts_mdl');
-            $check = $this->contacts_mdl->faq();
-            if ($check == true) {
-          echo "1";
-            }
-        } else {
-            echo "<div class='alert alert-danger'>" . validation_errors() . "</div>";
-        }
-    }
-    function contact()
-    {
-        $this->load->library('form_validation');
-        $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('phone', 'Mobile', 'required|trim|numeric|exact_length[10]');
-        if ($this->form_validation->run() == true) {
-            $this->load->model('contacts_mdl');
-            $check = $this->contacts_mdl->contact();
-            if ($check == true) {
-          echo "1";
-            }
-        } else {
-            echo "<div class='alert alert-danger'>" . validation_errors() . "</div>";
-        }
-    }
 
-   function complaint()
-    {
-        $this->load->library('form_validation');
-        $this->form_validation->set_rules('name', 'Name', 'required|trim');
-        $this->form_validation->set_rules('phone', 'Mobile', 'required|trim|numeric|exact_length[10]');
-        if ($this->form_validation->run() == true) {
-            $this->load->model('contacts_mdl');
-            $check = $this->contacts_mdl->complaint();
-            if ($check == true) {
-          echo "1";
-            }
-        } else {
-            echo "<div class='alert alert-danger'>" . validation_errors() . "</div>";
-        }
-    }
 
-    function newsletter()
-    {
-        $this->load->library('form_validation');
-        $this->form_validation->set_rules('email', 'email', 'required|trim');
-        if ($this->form_validation->run() == true) {
-            $this->load->model('contacts_mdl');
-            $check = $this->contacts_mdl->newsletter();
-            if ($check == true) {
-          echo "1";
-            }
-        } else {
-            echo "<div style='background:red !important;'>" . validation_errors() . "</div>";
-        }
-    }
-    
+    // function complaint()
+    // {
+    //     $this->load->library('form_validation');
+    //     $this->form_validation->set_rules('name', 'Name', 'required|trim');
+    //     $this->form_validation->set_rules('phone', 'Mobile', 'required|trim|numeric|exact_length[10]');
+    //     if ($this->form_validation->run() == true) {
+    //         $this->load->model('contacts_mdl');
+    //         $check = $this->contacts_mdl->complaint();
+    //         if ($check == true) {
+    //             echo "1";
+    //         }
+    //     } else {
+    //         echo "<div class='alert alert-danger'>" . validation_errors() . "</div>";
+    //     }
+    // }
 }
