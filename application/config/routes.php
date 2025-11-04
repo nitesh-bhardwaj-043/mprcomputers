afd4b26049544a79506924f8146878f8729f199a
nitesh-bhardwaj-043 <nitesh123bhardwaj123@gmail.com>
2025-11-05 01:00:51 +0530

    complaint form page done

--- application/config/routes.php
+++ application/config/routes.php
@@ -6,7 +6,7 @@ $route['search'] = 'home/search';
 $route["photo-gallery"]="gallery/photo_gallery";
 $route["(:any).htm"]="home/error";
 $route["contact-us"]="contacts/contacts";
-$route["complaint"]="complaint/contacts";
+$route["complaint"]="contacts/complaint";
 $route["packers-movers-(:any)-india"]="packers_movers/state_services/$1";
 $route["(:any)-packers-movers-(:any)"]="packers_movers/city/$2/$1";
 
